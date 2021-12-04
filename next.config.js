2849a32345990a46771944104f31e08b552bb077
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2021-12-05 02:21:48 +0700

    add redirects

--- next.config.js
+++ next.config.js
@@ -10,4 +10,13 @@ module.exports = {
 
     return config
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false,
+      },
+    ]
+  }
 }
