c92aeb356a4ebd7e58ae077b96005f37ea9bb15f
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2021-12-05 02:06:52 +0700

    add page charts

---
+++ pages/app/charts.tsx
@@ -0,0 +1,64 @@
+import ChartCard from 'components/Chart/ChartCard'
+import { Doughnut, Line, Bar } from 'react-chartjs-2'
+import ChartLegend from 'components/Chart/ChartLegend'
+import PageTitle from 'components/Typography/PageTitle'
+import Layout from 'containers/Layout'
+import {
+  doughnutOptions,
+  lineOptions,
+  barOptions,
+  doughnutLegends,
+  lineLegends,
+  barLegends,
+} from 'utils/demo/chartsData'
+import {
+  Chart,
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+
+function Charts() {
+  Chart.register(
+    ArcElement,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  )
+
+  return (
+    <Layout>
+      <PageTitle>Charts</PageTitle>
+
+      <div className="grid gap-6 mb-8 md:grid-cols-2">
+        <ChartCard title="Doughnut">
+          <Doughnut {...doughnutOptions} />
+          <ChartLegend legends={doughnutLegends} />
+        </ChartCard>
+
+        <ChartCard title="Lines">
+          <Line {...lineOptions} />
+          <ChartLegend legends={lineLegends} />
+        </ChartCard>
+
+        <ChartCard title="Bars">
+          <Bar {...barOptions} />
+          <ChartLegend legends={barLegends} />
+        </ChartCard>
+      </div>
+    </Layout>
+  )
+}
+
+export default Charts
